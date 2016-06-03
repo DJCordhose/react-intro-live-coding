a4deaa9f4da30fc295e72e307356bd0191ec431b
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2016-06-03 08:07:01 +0200

    details

--- src/step-1-typed/client/main.js
+++ src/step-1-typed/client/main.js
@@ -7,6 +7,6 @@ import HelloMessage from './HelloMessage';
 
 const mountNode = document.getElementById('mount');
 // ERROR: Must be a ReactElement
-// const element = 'just a string';
-const element = <HelloMessage greeting="Hello"/>;
+const element = 'just a string';
+// const element = <HelloMessage greeting="Hello"/>;
 ReactDOM.render(element, mountNode);
